47e887c0329cceba7f444318fb88230df3fe4696
Jochen <jochen.panjaer@ziango.com>
2020-01-09 22:48:53 +0100

    front-end setup for svelte and parcel

---
+++ client/src/js/components/app.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+
+<style>
+</style>
+
+
+<h1>I am loaded</h1>
---
+++ client/src/js/index.js
@@ -0,0 +1,6 @@
+import App from './components/app.svelte';
+
+const app = new App({
+    target: document.querySelector('#appContainer'),
+    props: {}
+});
